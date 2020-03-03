9cdff357e0345f0aa3702434caefe8890812ea9e
rrou <qq649492@gmail.com>
2020-03-03 18:02:14 +0800

    Shopping Cart - CheckBox(Progressing)

--- vip/Controller/ShoppingCartController.swift
+++ vip/Controller/ShoppingCartController.swift
@@ -10,11 +10,41 @@ import Foundation
 import UIKit
 import Firebase
 
-class ShoppingCartController : UIViewController {
+class ShoppingCartController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadData()
+    }
+   
+    func loadData(){
+        
+    }
+    
+    func numberOfSectionInTableView(tableView: UITableView) -> Int{
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        return cell
+    }
+    
+    
     
     @IBAction func checkBoxTapped(_ sender: UIButton){
         if sender.isSelected{
             print("Selected!")
+            sender.isSelected = false
+        }else{
+            sender.isSelected = true
         }
     }
     
