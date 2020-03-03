26848cc7727c072e178747ba63147e27367bbf39
rrou <qq649492@gmail.com>
2020-03-03 12:19:28 +0800

    CheckBoxButton(Progressing)

---
+++ vip/Controller/ShoppingCartController.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingCartController.swift
+//  vip
+//
+//  Created by rourou on 03/03/2020.
+//  Copyright © 2020 Ines. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class ShoppingCartController : UIViewController {
+    
+    @IBAction func checkBoxTapped(_ sender: UIButton){
+        if sender.isSelected{
+            print("Selected!")
+        }
+    }
+    
+}
