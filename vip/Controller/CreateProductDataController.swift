4e3372e36cfcbb8a5fd3b4368610e61de8f3b574
rrou <qq649492@gmail.com>
2020-03-05 00:24:26 +0800

    create product Information

--- vip/Controller/CreateProductDataController.swift
+++ vip/Controller/CreateProductDataController.swift
@@ -13,10 +13,32 @@ import Firebase
 
 class CreateProductDataController: UIViewController {
     
-   
+    @IBOutlet weak var ProductName: UITextField!
+    @IBOutlet weak var Price: UITextField!
+    @IBOutlet weak var Description: UITextField!
+    @IBOutlet weak var ProductEvaluation: UITextField!
+    @IBOutlet weak var SellerEvaluation: UITextField!
+    @IBOutlet weak var Notice: UITextField!
+    @IBOutlet weak var ManuDate: UITextField!
+    @IBOutlet weak var ExpDate: UITextField!
+    @IBOutlet weak var Method: UITextField!
+    @IBOutlet weak var OtherInfo: UITextField!
+    var ref: DatabaseReference!
+
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference()
+        
+    }
     
-    
+    @IBAction func CreatBtn(_ sender: Any) {
+        
+        let id = UUID()
+        print(ProductName.text)
+//        self.ref.child("Product/\(id)").setValue(["ProductName": ProductName.text ?? "Null", "Price": Price.text ?? "Null", "Description": Description.text ?? "Null", "ProductEvaluation": ProductEvaluation.text ?? "Null", "SellerEvaluation": SellerEvaluation.text ?? "Null", "Notice": Notice.text ?? "Null", "ManuDate": ManuDate.text ?? "Null", "ExpDate": ExpDate.text ?? "Null", "Method": Method.text ?? "Null", "OtherInfo": OtherInfo.text ?? "Null"])
+        
+    }
     
     
 }
