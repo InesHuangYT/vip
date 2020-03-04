1afa32d3c2d7888d043251c345ef7b40ad8ef2b3
rrou <qq649492@gmail.com>
2020-03-04 18:52:51 +0800

    CreateProductDataController

---
+++ vip/Controller/CreateProductDataController.swift
@@ -0,0 +1,22 @@
+//
+//  CreateProductDataController.swift
+//  vip
+//
+//  Created by rourou on 04/03/2020.
+//  Copyright © 2020 Ines. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+
+class CreateProductDataController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+}
