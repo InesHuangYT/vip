3b1376d0e1639d97f4d8732b079da2a929768cd4
inesbaby <inse52086@gmail.com>
2020-03-08 17:11:33 +0800

    Profile storyboard, Profile controller

---
+++ vip/Controller/ProfileController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileController.swift
+//  vip
+//
+//  Created by Ines on 2020/3/8.
+//  Copyright © 2020 Ines. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+}
