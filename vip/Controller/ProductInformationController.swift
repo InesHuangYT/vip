4b1583386c0a44e26457553dd0bcd5290a8f2e7f
inesbaby <inse52086@gmail.com>
2020-03-16 18:57:16 +0800

    add product, product information scene and controller

---
+++ vip/Controller/ProductInformationController.swift
@@ -0,0 +1,21 @@
+//
+//  ProductInformationController.swift
+//  vip
+//
+//  Created by Ines on 2020/3/16.
+//  Copyright © 2020 Ines. All rights reserved.
+//
+
+import UIKit
+
+class ProductInformationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+
+}
